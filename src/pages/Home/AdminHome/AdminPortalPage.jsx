21a38756a511484ccbe78e3f7f15a852259710c2
Julian Jimenez <julianjimenez2128@gmail.com>
2024-11-28 14:31:49 -0500

    feat(homepage): Home pages for administrators and employees -AdminPortalPage: Home portal for administrators. -EmployeePortalPage: Home portal for employees

---
+++ src/pages/Home/AdminHome/AdminPortalPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function AdminPortalPage() {
+  console.log("funcionadoo administrator");
+  return (
+    <div>
+     <p> Bien hecho!! :)</p>
+  </div>
+)
+}
+
+export default AdminPortalPage;
---
+++ src/pages/Home/EmployeeHome/EmployeePortalPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function EmployeePortalPage() {
+  console.log("funcionadoo Employee");
+  return (
+    <div>
+     <p> Bien hecho!! :)</p>
+  </div>
+)
+}
+
+export default EmployeePortalPage;
