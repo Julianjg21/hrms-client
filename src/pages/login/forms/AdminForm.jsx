e7df09ecef39f3c9b4129192dc35b6fcbd86dab9
Julian Jimenez <julianjimenez2128@gmail.com>
2024-11-28 14:28:29 -0500

    feat(Login Forms): add login forms -Admin Form: login form for administrators -Employees: login form for employees

--- src/pages/login/forms/AdminForm.jsx
+++ src/pages/login/forms/AdminForm.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function AdminForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); //We use the useNavigate hook
+  const handleLogin = async (event) => {
+    event.preventDefault(); //Prevent page reload
+    const typeUser = "administrator"; //type of user to enter
+    try {
+      //
+      const response = await axios.post("http://localhost:3080/auth/login", {
+        email,
+        password,
+        typeUser,
+      });
+
+      //Save the token in localStorage
+      localStorage.setItem("AdminToken", response.data.token);
+      //Navigate to the AdminPortal path
+      navigate("/AdminPortal");
+    } catch (error) {
+      console.error(
+        "Error al iniciar sesión:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
   return (
-    <Form className="p-3">
+    <Form className="p-3" onSubmit={handleLogin}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className="float-start">Correo</Form.Label>
-        <Form.Control type="email" placeholder="Ingrese su email registrado" />
+        <Form.Control
+          type="email"
+          placeholder="Ingrese su email registrado"
+          value={email} //Bind the state
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <Form.Text className="text-muted">
           No compartas tu contraseña personal.
         </Form.Text>
@@ -13,7 +46,12 @@ function AdminForm() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label className="float-start">Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Ingrese su contraseña" />
+        <Form.Control
+          type="password"
+          placeholder="Ingrese su contraseña"
+          value={password} //Bind the state
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
       <div className="d-flex justify-content-center mt-3 mb-3 ">
         <Button variant="light" type="submit" className="border border-1">
--- src/pages/login/forms/EmployeesForm.jsx
+++ src/pages/login/forms/EmployeesForm.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function EmployeesForm() {
+  const [identification, setIdentification] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); //We use the useNavigate hook
+  const handleLogin = async (event) => {
+    event.preventDefault(); //Prevent page reload
+    const typeUser = "employee";
+
+    try {
+      const response = await axios.post("http://localhost:3080/auth/login", {
+        identification,
+        password,
+        typeUser,
+      });
+
+      //Save the token in localStorage
+      localStorage.setItem("EmployeeToken", response.data.token);
+     //navigate to the Employee Portal route
+      navigate("/EmployeePortal");
+    } catch (error) {
+      console.error(
+        "Error al iniciar sesión:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
   return (
-    <Form className="p-3">
+    <Form className="p-3" onSubmit={handleLogin}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className="float-start">
           Número de Identificación
@@ -12,7 +40,9 @@ function EmployeesForm() {
 
         <Form.Control
           type="text"
+          value={identification} //Bind the state
           placeholder="Ingrese su número de identificación de identidad"
+          onChange={(e) => setIdentification(e.target.value)}
         />
         <Form.Text className="text-muted">
           No compartas tu contraseña personal.
@@ -21,7 +51,12 @@ function EmployeesForm() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label className="float-start">Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Ingrese su contraseña" />
+        <Form.Control
+          type="password"
+          placeholder="Ingrese su contraseña"
+          value={password} //Bind the state
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
       <div className="d-block justify-content-center mt-3">
         <Button
