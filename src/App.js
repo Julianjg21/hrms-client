4279f09dcf14d1e8c2344de7b647e1fdeebb6ae6
Julian Jimenez <julianjimenez2128@gmail.com>
2024-11-28 11:52:29 -0500

    feat : added react router and route guard -ProtectedRoutes: verifica el token almacenado y maneja el acceso a las rutas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import LoginPage from "./pages/login/LoginPage";
+import AdminPortalPage from "./pages/Home/AdminHome/AdminPortalPage";
 import "./App.css";
+import ProtectedRoute from "./services/Api/Auth/ProtectedRoute.mjs";
+import EmployeePortalPage from "./pages/Home/EmployeeHome/EmployeePortalPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Route for the login page */}
+        <Route path="/login" element={<LoginPage />} />
+        {/* Default route that also leads to the login page */}
         <Route path="/" element={<LoginPage />} />
-        {/*      <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> */}
+
+        {/* Protected route - only accessible if the user is authenticated */}
+        <Route element={<ProtectedRoute />}>
+          {/* Admin portal route, only accessible for admins */}
+          <Route path="/AdminPortal" element={<AdminPortalPage />} />
+          {/* Employee portal route, only accessible for employees */}
+          <Route path="/EmployeePortal" element={<EmployeePortalPage />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ src/services/Api/Auth/ProtectedRoute.mjs
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const ProtectedRoute = () => {
+  const [loading, setLoading] = useState(true); //To handle the load while validating the token
+  const [isValidToken, setIsValidToken] = useState(false); //To handle token validity
+  const navigate = useNavigate(); //Direct to another route
+  const location = useLocation(); //Get the current route
+
+  useEffect(() => {
+
+    let tokenKey = ''; //variable to store the token
+
+    //verify which route the client is trying to access to determine which access token it should verify
+    if (location.pathname.startsWith('/EmployeePortal')) {
+      tokenKey = 'EmployeeToken';
+    } else if (location.pathname.startsWith('/AdminPortal')) {
+      tokenKey = 'AdminToken';
+    }
+
+    //Get the token saved in the cookies
+    const token = localStorage.getItem(tokenKey);
+
+
+    if (!token) {
+     //If there is no token, redirect to login
+      navigate('/login');
+      setLoading(false);
+      return;
+    }
+
+    //Verify the token by sending it in the header
+    axios
+      .get('http://localhost:3080/auth/protected', {
+        headers: {
+          Authorization: `Bearer ${token}`,//Send the token in the header
+        },
+      })
+      .then(response => {
+        if (response.status === 200) {
+          setIsValidToken(true); //If the token is valid
+        }
+      })
+      .catch(() => {
+        localStorage.removeItem('authToken'); //If the token is invalid, remove the token
+        navigate('/login'); //Redirect to login if token is invalid
+      })
+      .finally(() => {
+        setLoading(false); //Stop loading when verification is finished
+      });
+  }, [navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isValidToken) {
+    return <Navigate to="/login" />; //If the token is invalid, redirect to the login
+  }
+  return <Outlet />; //If the token is valid, render the protected routes
+};
+
+export default ProtectedRoute;
