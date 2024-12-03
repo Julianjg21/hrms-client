5d1d323fb476095763879fde98069edf91204fbe
Julian Jimenez <julianjimenez2128@gmail.com>
2024-12-03 15:04:52 -0500

    feat(routes): add and update routes in App.js
    - Added new routes for dynamic rendering of components.
    - Updated existing routes to improve navigation and maintain structure.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import LoginPage from "./pages/login/LoginPage";
-import AdminPortalPage from "./pages/Home/AdminHome/AdminPortalPage";
+import AdminPortalPage from "./pages/home/adminHome/AdminPortalPage";
 import "./App.css";
-import ProtectedRoute from "./services/Api/Auth/ProtectedRoute.mjs";
-import EmployeePortalPage from "./pages/Home/EmployeeHome/EmployeePortalPage";
+import ProtectedRoute from "./services/api/auth/ProtectedRoute.mjs";
+import EmployeePortalPage from "./pages/home/employeeHome/EmployeePortalPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainUserManagement from "./pages/home/adminHome/AdminModules/userManagement/MainUserManagement";
 function App() {
   return (
     <Router>
       <Routes>
         {/* Route for the login page */}
         <Route path="/login" element={<LoginPage />} />
-        {/* Default route that also leads to the login page */}
-        <Route path="/" element={<LoginPage />} />
-
         {/* Protected route - only accessible if the user is authenticated */}
         <Route element={<ProtectedRoute />}>
           {/* Admin portal route, only accessible for admins */}
-          <Route path="/AdminPortal" element={<AdminPortalPage />} />
+          <Route path="/AdminPortal" element={<AdminPortalPage />}>
+            {/* child routes*/}
+            <Route path="userManagement" element={<MainUserManagement />} />
+          </Route>
           {/* Employee portal route, only accessible for employees */}
           <Route path="/EmployeePortal" element={<EmployeePortalPage />} />
         </Route>
