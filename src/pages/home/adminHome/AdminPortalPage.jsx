ad701b8f7112800757d91f8490700a7d34788292
Julian Jimenez <julianjimenez2128@gmail.com>
2024-12-04 07:56:27 -0500

    feat(pages):Add home page  to administrators

---
+++ src/pages/home/adminHome/AdminPortalPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HomeNavbar from "../../../components/navbar/HomeNavbar";
+import MainMenu from "../../../components/Menu/MainMenu";
+import AdminMenuButtons from "./adminMenuButtons/AdminMenuButtons";
+function AdminPortalPage() {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <header>
+        <HomeNavbar />
+      </header>
+      <main className="flex-grow-1 ">
+        <MainMenu MenuButtons={AdminMenuButtons} />
+      </main>
+      <div className="bg-dark mt-3" style={{height:"3rem"}}>
+      </div>
+    </div>
+  );
+}
+export default AdminPortalPage;
