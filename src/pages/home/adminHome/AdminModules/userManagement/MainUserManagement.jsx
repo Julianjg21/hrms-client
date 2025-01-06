ca2cf47b4d4aef5d1567e244a0f170b89237dcc2
Julian Jimenez <julianjimenez2128@gmail.com>
2025-01-06 16:33:44 -0500

    feat(MainUserManagement): implement dynamic button color change based on active route

--- src/pages/home/adminHome/AdminModules/userManagement/MainUserManagement.jsx
+++ src/pages/home/adminHome/AdminModules/userManagement/MainUserManagement.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-import CreateUser from "./userManagementSections/CreateUser";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 function MainUserManagement() {
+  //color of the buttons to change the section
+  const [createUserButtonColor, setCreateUserButtonColor] =
+    useState("text-bg-dark");
+  const [editUserButtonColor, setEditUserButtonColor] =
+    useState("text-bg-light");
+
+  const navigate = useNavigate();
+  const location = useLocation(); // useLocation hook
+
+  useEffect(() => {
+    const actualLocation = location.pathname; //Get the current route
+    //check the current route
+    if (actualLocation.includes("/editUser")) {
+      setCreateUserButtonColor("text-bg-light"); //set color to the admin form  button
+      setEditUserButtonColor("text-bg-dark"); // set color to the employee form button
+    } else {
+      setCreateUserButtonColor("text-bg-dark"); //set color to the admin form  button
+      setEditUserButtonColor("text-bg-light"); // set color to the employee form button
+    }
+  }, [location]); //react to changes
+
   return (
     <div className="container w-100">
       <div className="row ">
         <div className="col-6 p-0">
-          <Button variant="outline-dark" className="border-warning border-opacity-50 w-100 rounded-0" >Crear Usuario</Button>
+          <Button
+            variant="outline-dark"
+            className={`border-warning ${createUserButtonColor} border-opacity-50 w-100 rounded-0`}
+            onClick={() => navigate("createUser")}
+          >
+            Crear Usuario
+          </Button>
         </div>
         <div className="col-6 p-0">
-          <Button variant="outline-dark" className="border-warning w-100 rounded-0">Modificar Usuario</Button>
+          <Button
+            variant="outline-dark"
+            className={`border-warning w-100 ${editUserButtonColor} rounded-0`}
+            onClick={() => navigate("editUser")}
+          >
+            Modificar Usuario
+          </Button>
         </div>
       </div>
       <div>
-        <CreateUser />
+        <Outlet />
       </div>
     </div>
   );
