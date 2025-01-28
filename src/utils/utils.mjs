147ec6cea4852375f581bb9ed6e779e84e2e9138
Julian Jimenez <julianjimenez2128@gmail.com>
2025-01-27 22:25:16 -0500

    refactor(utils): improve permission selection logic and enhance code comments

--- src/utils/utils.mjs
+++ src/utils/utils.mjs
@@ -1,20 +1,33 @@
-
-//function to create object with permissions and their corresponding actions
+//function to create object with permits and their corresponding actions
 export const selectPermissions = (userAction) => {
-//Get all permissions from localStorage
+  //Get all permissions from localStorage
   const storedPermissions = localStorage.getItem("AllPermissions");
-//Parse the permissions to an array
-  const permissionsArray = JSON.parse(storedPermissions);
-//Filter the permissions that match the userAction
+
+//Verify if  storedPermissions is empty or not a valid array
+  if (!storedPermissions) {
+    return {}; //return an empty object if there are no stored permits
+  }
+
+ //Convert permissions into an array, or assign an empty array if the parso fails
+  const permissionsArray = JSON.parse(storedPermissions) || [];
+
+//Verify if userAction is empty or not a valid array
+  if (!userAction || !Array.isArray(userAction)) {
+    return {}; //Return an empty object if userAction is invalid
+  }
+
+//Filter the permits that coincide with the Userction
   const permissionFinded = permissionsArray.filter((item) =>
     userAction.includes(item.action)
   );
- //Convert the array to an object with action as key and permission as value
+
+//Convert the array to an object with the action as key and the permission as value
   const resultado = permissionFinded.reduce((acc, item) => {
     acc[item.action] = item.permission;
     return acc;
   }, {});
-//Return the object with the permissions
+
+  //Return the object with the permissions
   return resultado;
 };
 
