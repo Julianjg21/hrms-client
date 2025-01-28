d6b34ac1c43398af3c0c92fab3ee343d7030dd0e
Julian Jimenez <julianjimenez2128@gmail.com>
2025-01-27 22:22:08 -0500

    feat(SearchUsers): add user search functionality with modal display for results

---
+++ src/common/modals/searchUsers/ListUsersModal.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+function ListUsersModal({
+  showListUsersModal,
+  closeListUsersModal,
+  usersFound,
+}) {
+  const navigate = useNavigate(); //We use the useNavigate hook
+
+  //Navigate to the next route
+  const navigateNextRoute = (item) => {
+    navigate("/AdminPortal/userManagement/editUser", { state: item });
+  };
+  return (
+    <Modal
+      show={showListUsersModal}
+      onHide={closeListUsersModal}
+      centered
+      size="sm"
+    >
+      <Modal.Header className="border border-bottom-0" closeButton>
+        <Modal.Title className="fs-6 w-100 text-center">
+          ¡Usuario Encontrado!
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="border border-bottom-0 border-top-0 ">
+        <div className="text-center  mb-4">
+          {usersFound.user.map((item, index) => {
+            return (
+              <Button
+                className="w-100 mt-1 text-start rounded-4"
+                variant="outline-dark"
+                onClick={() => navigateNextRoute(item)}
+              >
+                {item.user_names} - {item.identification}{" "}
+                <span className="text-primary float-end">Ver</span>
+              </Button>
+            );
+          })}
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default ListUsersModal;
---
+++ src/components/users/SearchUsers.jsx
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from "react";
+import ProtectedElements from "../../services/api/auth/ProtectedElements.mjs";
+import ListUsersModal from "../../common/modals/searchUsers/ListUsersModal.jsx";
+import { Form, Card, Button, CardTitle } from "react-bootstrap";
+import { searchUsers } from "../../services/api/searchUsers/SearchUsersApis.mjs";
+import {
+  selectPermissions,
+  extractUsedPermissions,
+} from "../../utils/utils.mjs";
+import { searchEmployees } from "../../services/api/searchUsers/SearchUsersApis.mjs";
+function SearchUsers() {
+  const [showListUsersModal, setShowListUsersModal] = useState(false);
+
+  //User data states
+  const [userNames, setUserNames] = useState("");
+  const [identificationType, setIdentificationType] = useState(
+    "Cédula de Ciudadanía"
+  );
+  const [identification, setIdentification] = useState();
+  const [usersFound, setUsersFound] = useState({ user: [{}] });
+
+  //permissions and token
+  const [requiredPermissions, setRequiredPermissions] = useState([]); //Permissions required to access the component
+  const [permissions, setPermissions] = useState([]); //Permissions extracted from the required permissions
+  const [userId, setUserId] = useState(); //User id from  the admin logged in
+  const [token, setToken] = useState(); //Token from the admin logged in
+
+  //Show or close listUsersModal
+  const activateListUsersModal = () => setShowListUsersModal(true);
+  const closeListUsersModal = () => setShowListUsersModal(false);
+
+  useEffect(() => {
+    // First get the required permissions
+    const getPermissions = selectPermissions(["get_user_data"]);
+    // Then extract the required permissions
+    setRequiredPermissions(getPermissions);
+
+    // Then extract the permissions
+    const extract = extractUsedPermissions(getPermissions);
+    setPermissions(extract);
+    //Then get the user id
+    setUserId(localStorage.getItem("AdminUserId"));
+    //Then get the user token
+    setToken(localStorage.getItem("AdminToken"));
+  }, []);
+
+  const handleFindUser = async (e) => {
+    e.preventDefault();
+    const data = {
+      user_names: userNames, //User names
+      type_identification: identificationType, //Type of identification
+      identification: identification, //Identification number
+      permissions, //Permissions requied to access the component
+      userId, //User id from the admin logged in
+    };
+    try {
+      //Search the user with the data provided
+      const response = await searchUsers(data, token);
+      if (response.status === 200) {
+        //If the user is found
+        setUsersFound(response.data); //Save in the state  the user found
+        activateListUsersModal(); //Activate de modal with the list of users found
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //Function to search employees by type of area
+  const handleSeachEmployees = async (value) => {
+    const data = {
+      permissions,
+      userId,
+      employee_type: value,
+    };
+
+    try {
+      const response = await searchEmployees(data, token);
+      if (response.status === 200) {
+        setUsersFound(response.data); //Save in the state the users found
+        activateListUsersModal(); //Activate the modal with the list of users found
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="mt-3 mb-3">
+      <ProtectedElements requiredPermission={requiredPermissions.get_user_data}>
+        <Card className="text-center border border-dark ">
+          <Card.Body>
+            <CardTitle className="mt-4">Buscar usuario</CardTitle>
+
+            <Form onSubmit={handleFindUser}>
+              <div className="row mt-5">
+                <div className="col-md-2 col-1"></div>
+                <div className="col-md-8 col-10  ">
+                  <Form.Group controlId="formUserName">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Escribe aqui el nombre del usuario que buscas."
+                      className="rounded-4"
+                      onChange={(e) => setUserNames(e.target.value)}
+                    />
+                  </Form.Group>
+                </div>
+                <div className="col-md-2 col-1"></div>
+              </div>
+              <div className="row mt-4">
+                <div className="col-md-4 col-2"></div>
+                <div className="col-md-4 col-8">
+                  <Form.Group controlId="formDocumentSelect">
+                    <Form.Label>Tipo de Documento</Form.Label>
+                    <Form.Select
+                      className="text-center"
+                      value={identificationType}
+                      onChange={(e) => setIdentificationType(e.target.value)}
+                    >
+                      <option value="Cédula de Ciudadanía">
+                        Cédula de Ciudadanía
+                      </option>
+                      <option value="Cédula de Extranjería">
+                        Cédula de Extranjería
+                      </option>
+                      <option value="Documento de identificación nacional pasaporte">
+                        Documento de identificación nacional pasaporte
+                      </option>
+                      <option value="Pasaporte">Pasaporte</option>
+                    </Form.Select>
+                  </Form.Group>
+                </div>
+                <div className="col-4"></div>
+              </div>
+              <div className="row mt-4">
+                <div className="col-md-2 col 1"></div>
+                <div className="col-md-8 col-10">
+                  <Form.Group controlId="formDocumentNumber">
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Digite el número de identificación del usuario que busca."
+                      className="rounded-4"
+                      onChange={(e) => setIdentification(e.target.value)}
+                    />
+                  </Form.Group>
+                </div>
+                <div className="col-md-2 col-1"></div>
+              </div>
+              <Button
+                variant="light"
+                type="submit"
+                className="mt-4 border-black"
+              >
+                Buscar
+              </Button>
+              <div className="row mt-5">
+                <div className="col-1"></div>
+                <div className="col-10 border border-opacity-50 border-secondary  "></div>
+                <div className="col-1"></div>
+              </div>
+            </Form>
+
+            <div className="row mt-5 mb-4">
+              <div className="col-md-4 col-2"></div>
+              <div className="col-md-4 col-8">
+                <Form>
+                  <Form.Group className="form-group">
+                    <Form.Label htmlFor="exampleSelect" className="mb-5">
+                      Buscar Usuario por Tipo de <br /> Area
+                    </Form.Label>
+                    <Form.Select
+                      id="exampleSelect"
+                      className="form-control text-center"
+                      onChange={(e) => {
+                        handleSeachEmployees(e.target.value); // Llamar a la función
+                      }}
+                    >
+                      <option value="Seguridad">Área de Seguridad</option>
+                      <option value="Administrador">Área Administrativa</option>
+                      <option value="Limpieza">Área de Limpieza</option>
+                      <option value="Atencion">
+                        Área de Atención al Cliente
+                      </option>
+                      <option value="Entretenimiento">
+                        Área de Entretenimiento
+                      </option>
+                    </Form.Select>
+                  </Form.Group>
+                </Form>
+              </div>
+              <div className="col-md-4 col-2"></div>
+            </div>
+          </Card.Body>
+        </Card>
+      </ProtectedElements>
+      <ListUsersModal
+        showListUsersModal={showListUsersModal}
+        closeListUsersModal={closeListUsersModal}
+        usersFound={usersFound}
+      />
+    </div>
+  );
+}
+
+export default SearchUsers;
