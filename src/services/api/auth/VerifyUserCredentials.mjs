ef5e7fd645b6a8266cc12cbbe409c11e01793dc3
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-02 18:01:56 -0500

    feat(VerifyUserCredentials): add userId and permissions to password verification headers

--- src/services/api/auth/VerifyUserCredentials.mjs
+++ src/services/api/auth/VerifyUserCredentials.mjs
@@ -4,15 +4,20 @@ import { API_ROUTES } from "../../ApiEndPoints.mjs";
 // Function to verify the user password
 export const verifyUserPassword = async (
   data, //User Credentials and permissions
-  token //Token to authenticate the request
+  token, //Token to authenticate the request
+  permissions, //Permissions necessary to delete the user
+  userId, // userId of the admin
 ) => {
   try {
     const response = await axios.post(API_ROUTES.verifyPassword, data, {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
+        "x-user-id": userId, //user id of the admin
+        "x-permissions": JSON.stringify(permissions)//Convert object to String Json
       },
     });
+
     return response;
   } catch (error) {
     console.log("Error al intentar verificar la contraseña", error);
