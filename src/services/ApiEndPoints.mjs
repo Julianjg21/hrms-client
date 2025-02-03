86bb3f5313f8eba2619c6abbaa3498acafac2d12
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-03 18:02:14 -0500

    feat(ApiEndPoints): organize API routes and add user documentation endpoints

--- src/services/ApiEndPoints.mjs
+++ src/services/ApiEndPoints.mjs
@@ -1,16 +1,29 @@
 // Routes for the APIS
 export const API_ROUTES = {
+  //Login
   login: `${process.env.REACT_APP_API_BASE_URL}/auth/login`,
+
+  //Permissions
   getAllPermissions: `${process.env.REACT_APP_API_BASE_URL}/requestUserRole/getAllPermissions`,
+
+  //Reset password
   findEmail: `${process.env.REACT_APP_API_BASE_URL}/requestResetPassword/findEmail`,
   verifyCode: `${process.env.REACT_APP_API_BASE_URL}/requestResetPassword/verifyCode`,
   resetPassword: `${process.env.REACT_APP_API_BASE_URL}/requestResetPassword/resetPassword`,
+
+  //Search users
   searchUsers: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/searchUsers`,
   searchEmployees: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/searchEmployees`,
-  deleteUser: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/deleteUser`,
-  editUserData: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/editUserData`,
+
+  //Management users
   createUser: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/createUsers`,
+  editUserData: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/editUserData`,
+  deleteUser: `${process.env.REACT_APP_API_BASE_URL}/requestUserManagement/deleteUser`,
+
+  //Verify credentials
   verifyPassword: `${process.env.REACT_APP_API_BASE_URL}/auth/verifyUserPassword`,
+  
+  //user documentation
   uploadUserDocuments: `${process.env.REACT_APP_API_BASE_URL}/requestUserDocuments/uploadUserDocuments`,
   getUserDocuments: `${process.env.REACT_APP_API_BASE_URL}/requestUserDocuments/getUserDocuments`,
   downloadUserDocuments: `${process.env.REACT_APP_API_BASE_URL}/requestUserDocuments/downloadUserDocuments`,
