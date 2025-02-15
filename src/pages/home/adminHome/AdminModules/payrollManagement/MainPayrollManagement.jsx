a2d4eb64cc7d3a64c8c65a524734f41297c9bf97
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-14 19:43:06 -0500

    feat(PayrollManagement): Improve navigation and add buttons for payroll sections

--- src/pages/home/adminHome/AdminModules/payrollManagement/MainPayrollManagement.jsx
+++ src/pages/home/adminHome/AdminModules/payrollManagement/MainPayrollManagement.jsx
@@ -1,17 +1,67 @@
-import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-function MainPayrollManagement() {
-  const navigate = useNavigate(); // useNavigate hook
+import React, { useState, useEffect } from "react";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { IoMdCalculator } from "react-icons/io";
+import { FaFileAlt } from "react-icons/fa";
 
+import { Button } from "react-bootstrap";
+function MainPayrollManagement() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // State to keep track of the active button based on the current route
+  const [activeButton, setActiveButton] = useState("");
   useEffect(() => {
-    navigate("searchUsers", { state: "/AdminPortal/payrollManagement/payrollGenerator" });
-  }, []); //Navigate to the searchUsers component
+    navigate("searchUsersPg", {
+      state: "/AdminPortal/payrollManagement/payrollGenerator",
+    });
+  }, []);
+
+
+  // Update active button based on the current route
+  useEffect(() => {
+    setActiveButton(location.pathname);
+  }, [location]); //Navigate to the searchUsers component
+
+  const buttons = [
+    {
+      label: "Nomina",
+      icon: <IoMdCalculator className="text-secondary me-1" />,
+      path: "searchUsersPg",
+      state: "/AdminPortal/payrollManagement/payrollGenerator",
+    },
+    {
+      label: "Extractos de Nómina",
+      icon: <FaFileAlt className="text-secondary me-1" />,
+      path: "searchUsersPe",
+      state: "/AdminPortal/payrollManagement/downloadPayrollExtract",
+    },
+  ];
   return (
-    <div>
-      <Outlet />
+    <div className="container">
+      <div className="row ">
+        {buttons.map(({ label, icon, path, state }) => (
+          <div className="col-6 p-0 d-flex justify-content-center">
+            <Button
+              key={path}
+              title="Seleccionar sección"
+              variant="outline-dark"
+              className={`border-warning rounded-0 w-100  d-flex justify-content-center align-items-center ${
+                activeButton.includes(path) || activeButton.includes(state)
+                  ? "bg-dark text-light"
+                  : ""
+              }`}
+              onClick={() => navigate(path, { state })}
+            >
+              {label} {icon}
+            </Button>
+          </div>
+        ))}
+      </div>
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
   );
 }
-
 
 export default MainPayrollManagement;
