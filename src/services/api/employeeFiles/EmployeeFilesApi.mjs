27542a999f4e8f4bdc5fcaa0fb969df86a2c0f07
Julian Jimenez <julianjimenez2128@gmail.com>
2025-02-26 23:33:30 -0500

    feat(PayrollManagement): Add functions for generating and downloading payroll PDFs and extracts

--- src/services/api/employeeFiles/EmployeeFilesApi.mjs
+++ src/services/api/employeeFiles/EmployeeFilesApi.mjs
@@ -2,7 +2,12 @@ import axios from "axios";
 import { API_ROUTES } from "../../ApiEndPoints.mjs";
 
 // Function to upload user documents to the server
-export const uploadUserDocuments = async (formData, token, userId, permissions) => {
+export const uploadUserDocuments = async (
+  formData,
+  token,
+  userId,
+  permissions
+) => {
   try {
     // Sending file and metadata using FormData to the server
     const response = await axios.post(
@@ -12,8 +17,8 @@ export const uploadUserDocuments = async (formData, token, userId, permissions) 
         headers: {
           "Content-Type": "multipart/form-data", // Required header for file uploads
           Authorization: `Bearer ${token}`, // Bearer token for authorization
-          "x-user-id": userId,//user id of the admin
-          "x-permissions": JSON.stringify(permissions),//Convert object to String Json
+          "x-user-id": userId, //user id of the admin
+          "x-permissions": JSON.stringify(permissions), //Convert object to String Json
         },
       }
     );
@@ -52,6 +57,74 @@ export const downloadUserDocuments = async (data, token) => {
     // Requesting file download with specific headers and response type
     const response = await axios.get(
       `${API_ROUTES.downloadUserDocuments}/${data.documentId}`,
+      {
+        headers: {
+          "Content-Type": "application/json", // Header for JSON data
+          Authorization: `Bearer ${token}`, // Bearer token for authorization
+          "x-user-id": data.userId, //user id of the admin
+          "x-permissions": JSON.stringify(data.permissions), //Convert object to String Json
+        },
+        responseType: "blob", // Ensure binary data is returned
+      }
+    );
+    return response; // Return server response
+  } catch (error) {
+    console.log("Error downloading the file", error); // Log the error
+    throw error; // Propagate the error for further handling
+  }
+};
+// Function to create Payroll Pdf
+export const generatePayrollPdf = async (payrollData, token, data) => {
+  try {
+    // Requesting file download with specific headers and response type
+    const response = await axios.put(
+      `${API_ROUTES.generatePayrollPdf}/${payrollData.user_id}`,
+      { payrollData },
+      {
+        headers: {
+          "Content-Type": "application/json", // Header for JSON data
+          Authorization: `Bearer ${token}`, // Bearer token for authorization
+          "x-user-id": data.userId, //user id of the admin
+          "x-permissions": JSON.stringify(data.permissions), //Convert object to String Json
+        },
+        responseType: "arraybuffer", //Receive binary data
+      }
+    );
+    return response; // Return server response
+  } catch (error) {
+    console.log("Error creating and saving the pdf", error); // Log the error
+    throw error; // Propagate the error for further handling
+  }
+};
+
+// Function to retrieve user payroll extracts from the server
+export const getUserPayrollExtracts = async (data, token) => {
+  try {
+    // Requesting files from the server with necessary permissions and user info
+    const response = await axios.get(
+      `${API_ROUTES.getUserPayrollExtracts}?user_id=${data.user_id}`,
+      {
+        headers: {
+          "Content-Type": "application/json", // Header for JSON data
+          Authorization: `Bearer ${token}`, // Bearer token for authorization
+          "x-user-id": data.userId, //user id of the admin
+          "x-permissions": JSON.stringify(data.permissions), //Convert object to String Json
+        },
+      }
+    );
+    return response; // Return server response
+  } catch (error) {
+    console.log("Error obtaining files", error); // Log the error
+    throw error; // Propagate the error for further handling
+  }
+};
+
+// Function to download employee payroll extracts from the server
+export const downloadUserPayrollExtracts = async (data, token) => {
+  try {
+    // Requesting file download with specific headers and response type
+    const response = await axios.get(
+      `${API_ROUTES.downloadUserPayrollExtracts}/${data.payrollExtractId}`,
       {
         headers: {
           "Content-Type": "application/json", // Header for JSON data
