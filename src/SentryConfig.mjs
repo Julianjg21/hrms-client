b771f638c391f59480dc5024209776f7bdc171a8
Julian Jimenez <julianjimenez2128@gmail.com>
2025-03-15 19:57:35 -0500

    fix: Update Sentry environment variable to use REACT_APP_ENV

--- src/SentryConfig.mjs
+++ src/SentryConfig.mjs
@@ -4,5 +4,5 @@ import * as Sentry from "@sentry/react";
 // Initialize Sentry with configuration settings
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DNS, // Sentry Data Source Name (DSN) for tracking errors
-  environment: process.env.REACT_APP_NODE_ENV || "development", // Set the environment (production, development)
+  environment: process.env.REACT_APP_ENV || "development", // Set the environment (production, development)
 });
