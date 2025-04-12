f696b028093e44a5718b5ba8126b54fc38084c6a
Julian Jimenez <julianjimenez2128@gmail.com>
2025-04-11 20:20:42 -0500

    feat: Implement user event management API functions for create, update, delete, and retrieval

---
+++ src/services/api/userEvents/UserEventsApis.mjs
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { API_ROUTES } from "../../ApiEndPoints.mjs";
+import * as Sentry from "@sentry/react";
+
+//create new event
+export const createNewEvent = async (eventData, token, permissions) => {
+  try {
+    const response = await axios.post(API_ROUTES.createNewEvent, eventData, {
+      headers: {
+        Authorization: `Barear ${token}`,
+        "Content-Type": "application/json",
+        "x-user-id": eventData.userId, //user id of the user
+        "x-permissions": JSON.stringify(permissions), //Convert object to String Json
+      },
+    });
+    return response;
+  } catch (error) {
+    Sentry.captureException(error); // Capture the error in Sentry
+    throw error; // Handle error
+  }
+};
+
+//update event
+export const updateEvent = async (eventData, token, permissions, userId) => {
+  try {
+    const response = await axios.post(API_ROUTES.updateEvent, eventData, {
+      headers: {
+        Authorization: `Barear ${token}`,
+        "Content-Type": "application/json",
+        "x-user-id": userId, //user id of the user
+        "x-permissions": JSON.stringify(permissions), //Convert object to String Json
+      },
+    });
+    return response;
+  } catch (error) {
+    Sentry.captureException(error); // Capture the error in Sentry
+    throw error; // Handle error
+  }
+};
+
+//delete event
+export const deleteEvent = async (eventId, token, permissions, userId) => {
+  try {
+    const response = await axios.post(
+      `${API_ROUTES.deleteEvent}/eventId=${eventId}`,
+      {
+        headers: {
+          Authorization: `Barear ${token}`,
+          "Content-Type": "application/json",
+          "x-user-id": userId, //user id of the user
+          "x-permissions": JSON.stringify(permissions), //Convert object to String Json
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    Sentry.captureException(error); // Capture the error in Sentry
+    throw error; // Handle error
+  }
+};
+
+// Get all events of the user
+export const getAllEvents = async (token, permissions, userId) => {
+  try {
+    const response = await axios.post(
+      `${API_ROUTES.getAllEvents}/userId=${userId}`,
+      {
+        headers: {
+          Authorization: `Barear ${token}`,
+          "Content-Type": "application/json",
+          "x-user-id": userId, //user id of the user
+          "x-permissions": JSON.stringify(permissions), //Convert object to String Json
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    Sentry.captureException(error); // Capture the error in Sentry
+    throw error; // Handle error
+  }
+};
